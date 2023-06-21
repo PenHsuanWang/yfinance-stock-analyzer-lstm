71fa85af852f475318a883745963fe29df20f022
PenHsuanWang <willy118116@gmail.com>
2023-06-21 11:25:20 +0800

    fix torch model used device

--- src/trainer/train.py
+++ src/trainer/train.py
@@ -24,13 +24,14 @@ class PytorchTrainer(Trainer):
         self._criterion = criterion
         self._optimizer = optimizer
         self._device = device
-        self._training_data = training_data
-        self._training_labels = training_labels
+        self._training_data = training_data.to(self._device)
+        self._training_labels = training_labels.to(self._device)
 
     def run_training_loop(self, epochs: int) -> None:
         print(f"Training the model for {epochs} epochs")
         self._model.train()
         for epoch in range(epochs):
+            self._model.to(self._device)
             self._model.train()
             outputs = self._model(self._training_data)
             loss = self._criterion(outputs, self._training_labels)
